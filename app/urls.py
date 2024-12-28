5799ed7c576a94d17c5e78583cb539c0c7d7bb71
gledsonfagno <gleidsonfagner838@gmail.com>
2024-12-28 19:45:29 -0300

    Corrigir configuração para servir arquivos de mídia

--- app/urls.py
+++ app/urls.py
@@ -4,16 +4,14 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.http import HttpResponse
 
-# View simples para a página inicial
 def home(request):
     return HttpResponse("Bem-vindo ao Blog API!")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),  # Rota para a página inicial
-     path('api/', include('categories.urls')),
-    # path('api/', include('posts.urls')),  # Inclua as URLs do app `posts`
-] 
-# + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-if settings.DEBUG:  # Apenas no modo de desenvolvimento
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('', home),
+    path('api/', include('categories.urls')),
+]
+
+# Adicionar configuração para servir arquivos de mídia durante o desenvolvimento e produção
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
