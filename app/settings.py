2bae19c31f87faf6e86a6fb64447bf198d1e5061
gledsonfagno <gleidsonfagner838@gmail.com>
2024-12-30 16:34:33 -0300

    Refactor: Simplify settings for AWS S3 and Heroku deployment

--- app/settings.py
+++ app/settings.py
@@ -11,7 +11,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure--5n84+xhoige%ya!2f+55!%vpu8z82&p2tg2lw+(9mnp9r8ve&') 
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-# ALLOWED_HOSTS = ['btinews.herokuapp.com', 'localhost', '127.0.0.1']
 ALLOWED_HOSTS = ['*'] 
 
 INSTALLED_APPS = [
@@ -23,6 +22,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'categories',
+    'corsheaders',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -56,10 +57,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'app.wsgi.application'
 
-# DATABASES = {
-#     'default': dj_database_url.config(default='sqlite:///' + str(BASE_DIR / 'db.sqlite3'))
-# }
-
 DATABASES = { 'default': dj_database_url.config(default=os.getenv('DATABASE_URL')) }
 
 AUTH_PASSWORD_VALIDATORS = [
@@ -77,28 +74,42 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+# Configurações de internacionalização
 LANGUAGE_CODE = 'pt-br'
 TIME_ZONE = 'America/Sao_Paulo'
 USE_I18N = True
 USE_TZ = True
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-STATIC_URL = 'static/'
+# Arquivos estáticos (CSS, JavaScript)
+MEDIA_URL = 'media/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_URL = 'static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+
+# Configurações do AWS S3
 AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
-
+AWS_S3_FILE_OVERWRITE = False
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_S3_SIGNATURE_NAME = 's3v4',
 AWS_S3_REGION_NAME = 'us-east-2'
-AWS_S3_FILE_OVERWRITE = False
-AWS_DEFAULT_ACL =  None
-AWS_S3_VERIFY = True
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
+# Arquivos de mídia (imagens e uploads)
 MEDIA_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/'
-print("Bucket Name:", os.getenv("AWS_STORAGE_BUCKET_NAME"))
+
+STORAGES = {
+    # Media file (image) management   
+    "default": {
+        "BACKEND": "storages.backends.s3boto3.S3StaticStorage",
+    },
+    # CSS and JS file management
+    "staticfiles": {
+        "BACKEND": "storages.backends.s3boto3.S3StaticStorage",
+    },
+}
+
+
